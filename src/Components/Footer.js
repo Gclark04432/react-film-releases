feedc6668332607d7a3feb4dc171b6c9f9b439b2
Gary Clark <gclark04432@live.co.uk>
2020-03-02 16:15:05 +0000

    add some spacing on ul elements

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
   return (
-    <div>
-    <a href="https://www.imdb.com/calendar?region=GB&ref_=rlm">Hello
+    <div className="upcoming-releases-link">
+    <a href="https://www.imdb.com/calendar?region=GB&ref_=rlm">View More Upcoming Releases
       </a>
       </div>
     );
